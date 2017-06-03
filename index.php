011945d483f439773b2b6a92350534721274e35b
kornfeldc <kornfeld.c@gmail.com>
2017-06-03 12:55:07 +0200

    Initial

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+    	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    	<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta http-equiv="pragma" content="no-cache">
+		<!--<meta name="theme-color" content="#FAF911">
+
+		<title>Bilderer</title>
+	
+		<!-- jquery -->
+		<script src="bower_components/jquery/dist/jquery.min.js" type="text/javascript"></script>
+		
+		<!-- vue | https://vuejs.org/ -->
+		<script src="bower_components/vue/dist/vue.min.js" type="text/javascript"></script>
+
+		<!-- fontawesome | http://fontawesome.io/ -->
+		<script src="https://use.fontawesome.com/877ad47a09.js"></script>
+
+		<!-- raleway font -->
+		<!-- https://fonts.google.com/specimen/Raleway -->
+		<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet"/>
+
+		<!-- my styles -->
+		<link href="css/global.css" rel="stylesheet"/>
+    </head>
+	<body>
+        
+    </body>
+	
+</html>
